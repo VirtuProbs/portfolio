1bf8c2c27c90b024124f92b8ca9088cc27c5756d
Victhor <victhorgabriel00@live.com>
2025-03-18 20:21:27 -0300

    configurando o ambiente de desenvolvimento

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  reactHooks.configs["recommended-latest"],
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    "extends": [
+      "some-other-config-you-use",
+      "prettier"
+    ],
+    "rules": {
+      "indent": "error"
+    }
+  },
+];
