1ae457445ad55718d2291f0f303b2a500c858798
Victhor <victhorgabriel00@live.com>
2025-03-20 21:39:10 -0300

    estilizando a sidebar

--- src/containers/Sidebar/idex.tsx
+++ src/containers/Sidebar/idex.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descrição, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/VirtuProbs.png" />
+    <SidebarContainer>
+    <Avatar />
     <Titulo fontSize={20}>Victhor Gabriel</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16}>
+      VirtuProbs
+    </Paragrafo>
+    <Descrição tipo='principal' fontSize={12}>Engenheiro front-end</Descrição>
+    <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
